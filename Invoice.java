4fc95b63224402b10dd50a69b8a16b3e425a5bfa
anjanasai006 <yanjanasai@gmail.com>
2026-03-11 12:30:06 +0530

    Create Invoice entity class with fields and methods

---
+++ Invoice.java
@@ -0,0 +1,49 @@
+package com.klef.fsad.exam;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="invoice")
+public class Invoice {
+
+    @Id
+    private int id;
+
+    private String name;
+    private String date;
+    private String status;
+
+    public Invoice() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
